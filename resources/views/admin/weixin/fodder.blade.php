258356192929b0f6985a22451d17495f1aedcd2e
liyijiehaha <2632379213@qq.com>
2019-04-28 00:42:46 +0000

    1

--- resources/views/admin/weixin/fodder.blade.php
+++ resources/views/admin/weixin/fodder.blade.php
@@ -6,15 +6,9 @@
 <body>
 <div style="padding-left:500px;padding-top:300px">
     <form method="post" action="fodderAdd" enctype ="multipart/form-data">
-        <table>
-            <tr>
-                <td><input type="file" id="upload" name="img"></td>
-            </tr>
-            <tr>
-                <td><input type="submit"></td>
-            </tr>
-        </table>
-    </form>-----------------------------------------------------------------------
+        <input type="file" id="upload" name="img">
+        <input type="submit">
+    </form>
 </div>
 </body>
 </html>
